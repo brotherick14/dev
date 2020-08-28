e2aa7f2a55201de8f13802abb523c880e03bcc9e
ERICK JERONIMO <erickjeronimo1@gmail.com>
2020-08-27 19:02:26 -0600

    prueba de scss

---
+++ src/components/Texto.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../assets/styles/Custom.scss';
+
+function Valor(){
+    return (
+        <div>
+            <h1>PRUEBA SCSS</h1>
+        </div>
+    );
+}
+
+export default Valor;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Texto from './components/Texto';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Texto />
   </React.StrictMode>,
   document.getElementById('root')
 );
