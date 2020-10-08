bf973f7f6df5a9251ea4c491191edf48b7787e7c
ERICK JERONIMO <erickjeronimo1@gmail.com>
2020-10-08 17:29:38 -0600

    Updates

---
+++ precache-manifest.2a05353f84a882ef59bbe8987ad9b738.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eec37db508960433576911989753850b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ec66c92f48158d916ff",
+    "url": "/static/css/main.0d7930bf.chunk.css"
+  },
+  {
+    "revision": "96b8aca5c8a36590c0ce",
+    "url": "/static/js/2.afdb785f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.afdb785f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ec66c92f48158d916ff",
+    "url": "/static/js/main.f4d7818c.chunk.js"
+  },
+  {
+    "revision": "0d440a989055022a9cc4",
+    "url": "/static/js/runtime-main.0af15533.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
